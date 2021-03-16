54349394fe229790b03a924169c21c504c5a4c2d
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-16 08:26:07 -0700

    Project cards (#6)
    * added social icons
    
    * new resume
    
    * made cards
    
    * made cards again
    
    * fixed conflict
    
    * actually saving

---
+++ src/components/projectCard/ProjectCard.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable*/
+import React from 'react';
+import styles from './ProjectCard.css';
+
+function ProjectCard({project}) {
+  const {
+    title,
+    image,
+    livesite,
+    githubFE,
+    githubBE,
+    stack,
+    keyLibraries,
+    description,
+    contributions
+  } = project;
+
+  return (
+    <div className={styles.ProjectCard}>
+      <section>
+        <hr/>
+        <h1>{title}</h1>
+        <p><span className={styles.bigger} >Description: </span>{description}</p>
+        <a href={livesite}>{livesite}</a>
+        {githubFE
+          ? <p><span className={styles.bigger} >Front End Repo: </span>{githubFE}</p> 
+          : null}
+        {githubBE
+          ? <p><span className={styles.bigger} >Back End Repo: </span>{githubBE}</p>
+          : null}
+        <p><span className={styles.bigger} >STACK: </span>{stack.map(el => ` ${el} |`)}</p>
+        <p><span className={styles.bigger} >Key Libraries: </span>{keyLibraries.map(el => ` ${el} |`)}</p>
+        <ul><span className={styles.bigger} >Key Contributions:</span></ul>
+        {contributions.map(el => <li key={el}>{el}</li>)}
+      </section>
+      <img className={styles.projectImage} src={image}/>
+    </div>
+  );
+}
+
+export default ProjectCard;
