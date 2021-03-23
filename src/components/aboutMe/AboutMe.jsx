ec6d2aad281150c99f73e376cf460d10c596e3c9
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-22 22:05:26 -0700

    setup about me

---
+++ src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Mechanical Engineer ----> Software Engineer</p>
+    </div>
+  );
+}
+
+export default AboutMe;
