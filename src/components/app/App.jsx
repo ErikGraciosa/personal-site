f4d24dcc545817e0510937e19f23380744759ec6
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-25 18:30:58 -0700

    Internet image (#13)
    * added social icons
    
    * new resume
    
    * made cards
    
    * made cards again
    
    * fixed conflict
    
    * actually saving
    
    * added links
    
    * fix scale
    
    * fixed links again
    
    * fix images
    
    * media query
    
    * refine media query
    
    * resume update
    
    * setup about me
    
    * removed full links
    
    * fixed pipes on last element
    
    * added image

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProjectList from '../projectCard/ProjectList';
 import StaticData from '../staticData/StaticData';
 import Footer from '../footer/Footer';
+import SiteThumbnail from '../siteThumbnail/SiteThumbnail';
 import './reset.css';
 import './App.css';
 
 export default function App() {
   return (
     <>
-      <StaticData/>
-      <ProjectList/>
-      <Footer/>
+      <Router>
+        <StaticData/>
+        <Switch>
+          <Route exact path="/" component={ProjectList}/>
+          <Route
+            exact
+            path="/site-preview-thumbnail"
+            component={SiteThumbnail}/>
+        </Switch>
+        <Footer/>
+      </Router>
     </>
   );
 }
