c6da735f640fc15f4863ebd44b132e77c9c78c77
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-29 10:50:46 -0700

    About me (#17)
    * added social icons
    
    * new resume
    
    * made cards
    
    * made cards again
    
    * fixed conflict
    
    * actually saving
    
    * added links
    
    * fix scale
    
    * fixed links again
    
    * fix images
    
    * media query
    
    * refine media query
    
    * resume update
    
    * setup about me
    
    * removed full links
    
    * fixed pipes on last element
    
    * added image
    
    * thumbnail added
    
    * updated meta tags
    
    * fix thumbnail image size
    
    * meta image location changed
    
    * added sections, layout good
    
    * about me complete
    
    * about me and stack added

---
+++ src/fixtures/stack.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+export const stack = {
+  languages: ['Javascript', 'HTML', 'CSS'],
+  frameworks: ['React', 'Node.js', 'Express', 'PUG(Jade)', 'Redux'],
+  databases: ['PostgreSQL'],
+  testing: ['Jest'],
+  other: ['Github', 'Heroku', 'Netlify', 'VSCode', 'Postman', 'PGAdmin', 'Agile/Scrum', 'Miro', 'Jira', 'StripeAPI']
+};
